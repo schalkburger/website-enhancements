affd6e7d9cf5b45c7fd15a15e60e5ec6194ca76a
Schalk Burger <schalkb@gmail.com>
2023-08-18 18:09:24 +0200

    Update Know Your Meme userstyle and userscript

--- knowyourmeme-enhanced/knowyourmeme-enhanced.user.js
+++ knowyourmeme-enhanced/knowyourmeme-enhanced.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/schalkburger/website-enhancements
 // @include      https://knowyourmeme.com/memes/*
 // @match        https://knowyourmeme.com/memes/*
-// @version      1.0.0
+// @version      1.3.0
 // @author       Schalk Burger <schalkb@gmail.com>
 // @description  Know Your Meme Enhanced
 // @license MIT
@@ -15,11 +15,28 @@
   let version = GM_info.script.version;
   console.log(`Know Your Meme Enhanced ${version}`);
 
-  // Get all the <img> elements with the class "img-nsfw"
-  let imgElements = document.querySelectorAll("img.img-nsfw");
+  // Function to handle lazy loading images
+  function lazyLoadImages() {
+    // Get all the <img> elements with the class "img-nsfw"
+    let imgElements = document.querySelectorAll("img.img-nsfw");
 
-  // Loop through each <img> element and replace the src attribute
-  imgElements.forEach(function (imgElement) {
-    imgElement.src = imgElement.getAttribute("data-original-image-url");
-  });
+    // Loop through each <img> element and check if it's in the viewport
+    imgElements.forEach(function (imgElement) {
+      if (isElementInViewport(imgElement)) {
+        imgElement.src = imgElement.getAttribute("data-original-image-url");
+      }
+    });
+  }
+
+  // Function to check if an element is in the viewport
+  function isElementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+    return rect.bottom >= 0 && rect.right >= 0 && rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.left <= (window.innerWidth || document.documentElement.clientWidth);
+  }
+
+  // Initial lazy load
+  lazyLoadImages();
+
+  // Listen for the "scroll" event and lazy load images as the user scrolls
+  window.addEventListener("scroll", lazyLoadImages);
 })();
